04a91f3a0c7c6a64cb43d066decc29949686880c
CamaradaPro7 <pmartin3312@gmail.com>
2026-06-25 17:52:03 +0200

    Actualizar app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-// app.js
+const comidas = [
+    "🍳 Desayuno",
+    "🍝 Comida",
+    "🥪 Merienda",
+    "🌙 Cena"
+];
+
+const contenedor = document.getElementById("meals");
+
+comidas.forEach(nombre => {
+
+    contenedor.innerHTML += `
+        <section class="card">
+
+            <h2>${nombre}</h2>
+
+            <button class="addButton">
+                ＋ Añadir alimento
+            </button>
+
+        </section>
+    `;
+
+});
