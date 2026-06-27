e11da591e2dd8f44a1bb9aa4dc29f52954f9398e
CamaradaPro7 <pmartin3312@gmail.com>
2026-06-27 16:27:43 +0200

    Crear biblioteca.js

---
+++ js/biblioteca.js
@@ -0,0 +1,25 @@
+// Biblioteca de alimentos - Mi Nutrición V2
+
+let biblioteca = JSON.parse(localStorage.getItem("biblioteca")) || [];
+
+function guardarBiblioteca() {
+    localStorage.setItem("biblioteca", JSON.stringify(biblioteca));
+}
+
+function obtenerBiblioteca() {
+    return biblioteca;
+}
+
+function agregarAlimento(alimento) {
+    const existe = biblioteca.find(
+        item => item.nombre.toLowerCase() === alimento.nombre.toLowerCase()
+    );
+
+    if (existe) {
+        return false;
+    }
+
+    biblioteca.push(alimento);
+    guardarBiblioteca();
+    return true;
+}
