25a52add63af0be42d281dac5bc095f1487a2629
CamaradaPro7 <pmartin3312@gmail.com>
2026-07-03 22:48:10 +0200

    Crear db.js

---
+++ js/db.js
@@ -0,0 +1,34 @@
+const DB_NAME = "MiNutricionDB";
+const DB_VERSION = 1;
+
+let db;
+
+function openDB(){
+
+    return new Promise((resolve,reject)=>{
+
+        const request = indexedDB.open(DB_NAME,DB_VERSION);
+
+        request.onupgradeneeded = e=>{
+
+            db = e.target.result;
+
+            db.createObjectStore("foods");
+            db.createObjectStore("meals");
+            db.createObjectStore("settings");
+
+        };
+
+        request.onsuccess = e=>{
+
+            db = e.target.result;
+
+            resolve(db);
+
+        };
+
+        request.onerror = ()=>reject(request.error);
+
+    });
+
+}
