5bc119830fce7ac7b350a11a200b1a816bc391f2
CamaradaPro7 <pmartin3312@gmail.com>
2026-07-04 10:02:03 +0200

    Actualizar db.js

--- js/db.js
+++ js/db.js
@@ -3,38 +3,78 @@ const DB_VERSION = 1;
 
 let db;
 
-function openDB(){
+function openDB() {
+    return new Promise((resolve, reject) => {
 
-    return new Promise((resolve,reject)=>{
+        const request = indexedDB.open(DB_NAME, DB_VERSION);
 
-        const request = indexedDB.open(DB_NAME,DB_VERSION);
-
-        request.onupgradeneeded = e=>{
-
-            db = e.target.result;
-
-            db.createObjectStore("foods");
-            db.createObjectStore("meals");
-            db.createObjectStore("settings");
-
-        };
-
-        request.onsuccess = e=>{
+        request.onupgradeneeded = e => {
 
             db = e.target.result;
 
-            resolve(db);
+            if (!db.objectStoreNames.contains("data")) {
+                db.createObjectStore("data");
+            }
 
         };
 
-        request.onerror = ()=>reject(request.error);
+        request.onsuccess = e => {
+            db = e.target.result;
+            resolve();
+        };
+
+        request.onerror = reject;
+
+    });
+}
+
+async function get(key) {
+
+    await openDB();
+
+    return new Promise(resolve => {
+
+        const tx = db.transaction("data", "readonly");
+        const store = tx.objectStore("data");
+
+        const req = store.get(key);
+
+        req.onsuccess = () => resolve(req.result);
+
+        req.onerror = () => resolve(null);
 
     });
 
 }
 
-openDB().then(()=>{
+async function set(key, value) {
 
-    console.log("✅ IndexedDB lista");
+    await openDB();
 
-});
+    return new Promise(resolve => {
+
+        const tx = db.transaction("data", "readwrite");
+
+        tx.objectStore("data").put(value, key);
+
+        tx.oncomplete = resolve;
+
+    });
+
+}
+
+window.DB = {
+
+    getFoods: () => get("foods"),
+
+    saveFoods: data => set("foods", data),
+
+    getMeals: () => get("meals"),
+
+    saveMeals: data => set("meals", data),
+
+    getSettings: () => get("settings"),
+
+    saveSettings: data => set("settings", data)
+
+};
