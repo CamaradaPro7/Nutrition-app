a3a1a9eede4b3e4e955f69fe6fd08e8c384ce25e
CamaradaPro7 <pmartin3312@gmail.com>
2026-07-05 18:45:52 +0200

    Actualizar app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,54 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded",()=>{
+/* ==========================================================
+   MI NUTRICIÓN NEXT V7
+   app.js
+========================================================== */
 
-    console.log("Mi Nutrición V7");
+const App={
 
-});
+    version:"7.0",
+
+    state:{
+
+        currentDate:DB.todayKey(),
+
+        settings:{
+            kcalGoal:2200
+        },
+
+        foods:[],
+
+        meals:DB.emptyDay()
+
+    },
+
+    async init(){
+
+        console.log(
+
+            "🚀 Iniciando Mi Nutrición NEXT V7..."
+
+        );
+
+        await DB.open();
+
+        console.log(
+
+            "✅ Base de datos preparada"
+
+        );
+
+        Dashboard.render();
+
+    }
+
+};
+
+document.addEventListener(
+
+    "DOMContentLoaded",
+
+    ()=>App.init()
+
+);
