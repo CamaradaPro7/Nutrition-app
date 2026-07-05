dcdfe2fa476453e71b213ff21f541d7285c86100
CamaradaPro7 <pmartin3312@gmail.com>
2026-07-05 19:19:30 +0200

    Actualizar biblioteca.js

--- js/biblioteca.js
+++ js/biblioteca.js
@@ -1,1 +1,51 @@
-a
+"use strict";
+
+const Biblioteca={
+
+foods:[],
+
+render(){
+
+console.log("📚 Biblioteca iniciada");
+
+},
+
+add(food){
+
+this.foods.push(food);
+
+},
+
+all(){
+
+return this.foods;
+
+},
+
+find(id){
+
+return this.foods.find(
+
+food=>food.id===id
+
+);
+
+},
+
+exists(name){
+
+return this.foods.find(
+
+food=>
+
+normalize(food.name)===
+
+normalize(name)
+
+);
+
+}
+
+};
+
+console.log("✅ biblioteca.js cargado");
