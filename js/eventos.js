cf10947617bc810b90a6461ad17904bd1f825e55
CamaradaPro7 <pmartin3312@gmail.com>
2026-07-05 19:21:17 +0200

    Actualizar eventos.js

--- js/eventos.js
+++ js/eventos.js
@@ -1,1 +1,45 @@
-a
+"use strict";
+
+const Eventos={
+
+init(){
+
+document
+
+.querySelectorAll(".meal")
+
+.forEach(card=>{
+
+card.addEventListener(
+
+"click",
+
+()=>{
+
+const meal=
+
+card.dataset.meal;
+
+Eventos.openMeal(meal);
+
+}
+
+);
+
+});
+
+},
+
+openMeal(meal){
+
+alert(
+
+`Has pulsado:\n\n${meal}`
+
+);
+
+}
+
+};
+
+console.log("✅ eventos.js cargado");
